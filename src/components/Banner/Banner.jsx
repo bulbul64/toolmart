2487ee1f1cd9cd2fe21e7575fc69ce5425aeee16
bulbul64 <bbb46483@gmail.com>
2026-03-30 00:29:17 +0600

    feat: initial commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import banner from "../../assets/banner.png";
 
 export default function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div className='hero bg-base-200 my-10'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <img
+          src={banner}
+          className='max-w-sm rounded-lg shadow-2xl'
+        />
+        <div>
+          <p>New: AI-Powered Tools Available</p>
+          <h1 className='text-5xl font-bold'>Box Office News!</h1>
+          <p className='py-6'>
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <button className='btn btn-primary'>Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,22 @@
-import Banner from "../components/Banner/Banner";
-import Footer from "../components/Footer/Footer";
-import Navbar from "../components/Navbar/Navbar";
-import MainSection from "../sections/MainSection";
+import Banner from '../components/Banner/Banner';
+import Footer from '../components/Footer/Footer';
 
+import MainSection from '../sections/MainSection';
 
 export default function MainLayout() {
   return (
-    <div className="container mx-auto">
-          <Navbar />
-          <Banner />
-          <MainSection />
-          <Footer />
-    </div>
-  )
+    <>
+      <div className=' border-b border-gray-200'>
+        <div className='container mx-auto'>
+        
+        </div>
+      </div>
+
+      <div className='container mx-auto px-2'>
+        <Banner />
+      </div>
+      <MainSection />
+      <Footer />
+    </>
+  );
 }
