5ed04e6fd2bf88fcfcc5158db29ac7aa6e6ac15e
bulbul64 <bbb46483@gmail.com>
2026-03-31 12:44:14 +0600

    fix

--- src/sections/Started.jsx
+++ src/sections/Started.jsx
@@ -1,5 +1,5 @@
 import { useFetchData } from "../api/api";
-import Card from "../components/card";
+import Card from "../components/Card";
 import { Button } from "../components/ui/button";
 import { Spinner } from "../components/ui/spinner";
 
